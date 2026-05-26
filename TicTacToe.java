8cc37430ebfb570238ba16f8282d15dbf51077d2
noorlatif2009 <noorlatif2109@gmail.com>
2026-05-26 05:54:23 -0700

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class TicTacToe {
@@ -9,7 +11,11 @@ public class TicTacToe {
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
     
-    JPanel[][] board = new JPanel[3][3];
+    JPanel boardPanel = new JPanel();
+    JButton[][] board = new JButton[3][3]; // to keep track of each button
+    String playerX = "X";
+    String playerO = "O";
+    String currentPlayer = playerX;
 
     public TicTacToe() {
         // Creates Window settings (i.e. size, resizibility)
@@ -28,16 +34,37 @@ public class TicTacToe {
         textLabel.setText("Tic Tac Toe");
         textLabel.setOpaque(true);
 
-        //
+        // Pushes the header at the top of the window
         textPanel.setLayout(new BorderLayout());
         textPanel.add(textLabel);
         frame.add(textPanel, BorderLayout.NORTH);
 
-        for (int r = 0; r < board.length; r++){
-            for (int c = 0; c < board[0].length; c++) {
-                board[r][c] = new JPanel();
-                board[r][c].setBackground(Color.darkGray);
-                frame.add(board[r][c]);
+        // Creates the buttons
+        boardPanel.setLayout(new GridLayout(3, 3));
+        boardPanel.setBackground(Color.darkGray);
+        frame.add(boardPanel);
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                // Makes tile
+                JButton tile = new JButton();
+                board[r][c] = tile;
+                boardPanel.add(tile);
+
+                // Sets tile visuals
+                tile.setBackground(Color.darkGray);
+                tile.setForeground(Color.BLACK);
+                tile.setFont(new Font ("Arial", Font.BOLD, 120));
+                tile.setFocusable(false);
+                tile.setContentAreaFilled(false);
+                tile.setOpaque(true);
+
+                // checks actions
+                tile.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        JButton tile = e.getSource();
+                    }
+                });
             }
         }
     }
